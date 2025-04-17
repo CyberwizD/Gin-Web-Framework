4476860584abb92e3845e6073d0a8f3a7e0077fc
WI$DOM <141811303+CyberwizD@users.noreply.github.com>
2025-04-17 23:39:13 +0100

    fix: correct comment documentation and formatting in Logger middleware

--- Custom Logger/main.go
+++ Custom Logger/main.go
@@ -9,10 +9,12 @@ import (
 
 func main() {
 	router := gin.New()
+
 	// LoggerWithFormatter middleware will write the logs to gin.DefaultWriter
 	// By default gin.DefaultWriter = os.Stdout
+
 	router.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
-		// your custom format
+		// Your custom format
 		return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s \"%s\" %s\"\n",
 			param.ClientIP,
 			param.TimeStamp.Format(time.RFC1123),
@@ -25,9 +27,12 @@ func main() {
 			param.ErrorMessage,
 		)
 	}))
+
 	router.Use(gin.Recovery())
+
 	router.GET("/ping", func(c *gin.Context) {
 		c.String(200, "pong")
 	})
+
 	router.Run(":8080")
 }

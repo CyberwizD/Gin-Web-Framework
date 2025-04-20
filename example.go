e30d80a13874c66ca2039dcef635792849623cfe
WI$DOM <141811303+CyberwizD@users.noreply.github.com>
2025-04-21 00:19:44 +0100

    feat: add POST /register endpoint

--- example.go
+++ example.go
@@ -17,5 +17,23 @@ func main() {
 		})
 	})
 
+	r.POST("/register", func(c *gin.Context) {
+		var json struct {
+			Username string `json:"username"`
+			Password string `json:"password"`
+		}
+
+		if err := c.ShouldBindJSON(&json); err != nil {
+			c.JSON(400, gin.H{"error": err.Error()})
+			return
+		}
+
+		if json.Username == "admin" && json.Password == "password" {
+			c.JSON(200, gin.H{"status": "logged in"})
+		} else {
+			c.JSON(401, gin.H{"status": "unauthorized"})
+		}
+	})
+
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
